99eadd67fc1c801b75edf3a0ab8ce53de16480eb
Thiago Rodrigues <businessthiagorodrigues@gmail.com>
2026-03-06 13:54:52 -0300

    todolist

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,66 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
+
+
+type Todo = {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
 export default function Home() {
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [input, setInput] = useState("");
+
+  const tarefa = () => {
+    const texto = input.trim();
+    if(!texto) return;
+
+    const novaTarefa: Todo = {
+      id: Date.now(),
+      text: texto,
+      completed: false
+    };
+
+    setTodos([...todos, novaTarefa]);
+    setInput("");
+
+  };
+
+  const deletaTarefa = (id: number) => {
+      setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const concluiTarefa = (id: number) => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? {...todo, completed: !todo.completed} : todo
+    ));
+  };
+
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 ">
+      <div className="bg-white shadow-lg rounded-3xl p-16">
+        <h1 className="text-3xl font-bold text-center mb-6">ToDo List</h1>
+        <div className="mb-4 flex">
+          <input value={input} onChange={(e) => setInput(e.target.value)}
+           type="text" className="flex px-3 py-2 border rounded-l-lg focus:outline-none focus:ring-2
+            focus:ring-blue-500" placeholder="adicionar uma tarefa"/>
+
+          <button onClick={tarefa} className="bg-blue-500 text-white px-4 py-2 rounded-r-lg hover:bg-blue-600">Adicionar</button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <ul className="space-y-2">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input type="radio" onClick={() => concluiTarefa(todo.id)} className="mr-2" checked={todo.completed}/>
+              {todo.text} - {todo.completed ? "Concluído" : "Pendente"}
+              <button onClick={() => deletaTarefa(todo.id)} className=" bg-red-500 text-white py-1 hover:bg-red-600">Deletar</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      
     </div>
   );
 }
